ab301a37d96053f920bf2ce427735181a0bc9006
Ricardo Cambui <r.cambui@hotmail.com>
2023-05-09 17:18:31 -0300

    3 exercícios adicionados

--- src/iniciante/java/Beecrowd1012.java
+++ src/iniciante/java/Beecrowd1012.java
@@ -22,7 +22,7 @@ public class Beecrowd1012 {
 
     public static void main(String[] args) throws IOException {
         Scanner scan = new Scanner(System.in);
-        
+
         double a = scan.nextDouble();
         double b = scan.nextDouble();
         double c = scan.nextDouble();
