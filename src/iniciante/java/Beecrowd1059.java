6c6cee2574d2c1c2979910c6edfc5bf4dfee974a
Ricardo Cambui <r.cambui@hotmail.com>
2023-05-11 09:25:04 -0300

    3 exercícios adicionados

---
+++ src/iniciante/java/Beecrowd1059.java
@@ -0,0 +1,24 @@
+package iniciante.java;
+
+/*Faça um programa que mostre os números pares entre 1 e 100, inclusive.
+
+Entrada
+Neste problema extremamente simples de repetição não há entrada.
+
+Saída
+Imprima todos os números pares entre 1 e 100, inclusive se for o caso, um em cada linha.*/
+
+import java.io.IOException;
+
+public class Beecrowd1059 {
+
+    public static void main(String[] args) throws IOException {
+
+        for(int i = 1; i <= 100; ++i) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+
+    }
+}
