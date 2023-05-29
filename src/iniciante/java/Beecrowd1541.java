3ba853f37ce4684191faffec61eebe3ae02e541d
Ricardo Cambui <r.cambui@hotmail.com>
2023-05-29 16:49:38 -0300

    Update Beecrowd1541.java

--- src/iniciante/java/Beecrowd1541.java
+++ src/iniciante/java/Beecrowd1541.java
@@ -1,2 +1,22 @@
-package iniciante.java;public class Beecrowd1541 {
+package iniciante.java;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Beecrowd1541  {
+    public static void main(String[] args) throws IOException {
+        Scanner scan = new Scanner(System.in);
+        double A, B, C, X;
+
+        while ((A = scan.nextInt()) != 0) {
+            B = scan.nextInt();
+            C = scan.nextInt();
+            X = Math.sqrt((((A * B) / C) * 100.0));
+
+            System.out.println((int)X);
+        }
+
+        scan.close();
+    }
+
 }
