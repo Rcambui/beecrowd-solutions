08426b30dc132fb7802849c1608a9f11dcab42ae
Ricardo Cambui <r.cambui@hotmail.com>
2023-05-30 20:08:00 -0300

    Exercício Resolvido

--- src/iniciante/java/Beecrowd1564.java
+++ src/iniciante/java/Beecrowd1564.java
@@ -1,2 +1,37 @@
-package iniciante.java;public class Beecrowd1564 {
+package iniciante.java;
+
+/*O Brasil é o país sede da copa esse ano. Porém, há muitas pessoas protestando contra o governo.
+Em redes sociais é possível ver pessoas afirmando que não vai ter copa devido aos protestos.
+
+Mas esses rumores de que não haverá copa são totalmente falsos, a presidente Dilma Roussef
+já avisou: vai ter copa sim, e se reclamar vai ter duas!
+
+Entrada
+A entrada contém vários casos de teste e termina com EOF. Cada caso de teste consiste de uma
+linha contendo o número N de reclamações sobre a copa encaminhadas para a presidente (0 ≤ N ≤ 100).
+
+Saída
+Para cada teste, a saída consiste de uma linha dizendo "vai ter copa!" caso não haja reclamações
+para a presidente. Caso haja reclamações, a saída deverá dizer "vai ter duas!".*/
+
+import java.io.IOException;
+import java.util.Scanner;
+public class Beecrowd1564 {
+
+    public static void main(String[] args) throws IOException {
+        Scanner scan = new Scanner(System.in);
+
+        while (scan.hasNext()) {
+            int N = scan.nextInt();
+
+            if (N == 0) {
+                System.out.println("vai ter copa!");
+            }
+            else {
+                System.out.println("vai ter duas!");
+            }
+        }
+
+        scan.close();
+    }
 }
