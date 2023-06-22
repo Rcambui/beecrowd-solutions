18bda94b73d9889c2ab7a688de51f41a1aa58e28
Ricardo Cambui <r.cambui@hotmail.com>
2023-06-22 08:51:42 -0300

    Exercício c# adicionado

---
+++ src/iniciante/c#/Beecrowd1004.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+/*Leia dois valores inteiros. A seguir, calcule o produto entre 
+ * estes dois valores e atribua esta operação à variável PROD. 
+ * A seguir mostre a variável PROD com mensagem correspondente.   
+
+Entrada
+O arquivo de entrada contém 2 valores inteiros.
+
+Saída
+Imprima a mensagem "PROD" e a variável PROD conforme exemplo abaixo,
+com um espaço em branco antes e depois da igualdade. Não esqueça de
+imprimir o fim de linha após o produto, caso contrário seu programa
+apresentará a mensagem: “Presentation Error”.*/
+
+namespace Beecrowd1004;
+class Beecrowd1004
+{
+    static void Main(string[] args)
+    {
+        int A = int.Parse(Console.ReadLine());
+        int B = int.Parse(Console.ReadLine());
+
+        int prod = A * B;
+        Console.WriteLine($"PROD = {prod}");
+    }
+}
+
