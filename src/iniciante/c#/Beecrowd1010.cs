e4ffb688edf538558a4e8693f8cc15c95ba08aa3
Ricardo Cambui <r.cambui@hotmail.com>
2023-06-23 09:06:30 -0300

    Exercício 1010 em c# adicionado

---
+++ src/iniciante/c#/Beecrowd1010.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+/*Neste problema, deve-se ler o código de uma peça 1, o número de peças 1, o 
+ * valor unitário de cada peça 1, o código de uma peça 2, o número de peças 2 e 
+ * o valor unitário de cada peça 2. Após, calcule e mostre o valor a ser pago.
+
+Entrada
+O arquivo de entrada contém duas linhas de dados. Em cada linha haverá 3
+valores, respectivamente dois inteiros e um valor com 2 casas decimais.
+
+Saída
+A saída deverá ser uma mensagem conforme o exemplo fornecido abaixo, lembrando
+de deixar um espaço após os dois pontos e um espaço após o "R$". O valor deverá
+ser apresentado com 2 casas após o ponto.*/
+
+namespace Beecrowd1010;
+class Beecrowd1010
+{
+    static void Main(string[] args)
+    {
+        string[] info = Console.ReadLine().Split(' ');
+        string[] info2 = Console.ReadLine().Split(' ');
+
+        int quantidade1 = int.Parse(info[1]);
+        double preco1 = double.Parse(info[2]);
+
+        int quantidade2 = int.Parse(info2[1]);
+        double preco2 = double.Parse(info2[2]);
+
+        double valorPagar = (quantidade1 * preco1) + (quantidade2 * preco2);
+
+        Console.WriteLine("VALOR A PAGAR: R$ {0:F2}", valorPagar);
+    }
+}
+
