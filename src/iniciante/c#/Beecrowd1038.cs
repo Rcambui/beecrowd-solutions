4fb9b1d7132c5bc1a9c9043b631db9d7ee4d4260
Ricardo Cambui <r.cambui@hotmail.com>
2023-06-30 08:49:50 -0300

    Exercício 1038 em c# adicionado

---
+++ src/iniciante/c#/Beecrowd1038.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+/*Com base na tabela abaixo, escreva um programa que leia o código de um item e 
+ * a quantidade deste item. A seguir, calcule e mostre o valor da conta a pagar.
+
+Entrada
+O arquivo de entrada contém dois valores inteiros correspondentes ao código e
+à quantidade de um item conforme tabela acima.
+
+Saída
+O arquivo de saída deve conter a mensagem "Total: R$ " seguido pelo valor a ser
+pago, com 2 casas após o ponto decimal.*/
+
+namespace Beecrowd1038;
+class Beecrowd1038
+{
+    static void Main(string[] args)
+    {
+        string[] linha = Console.ReadLine().Split(' ');
+        int codigo = int.Parse(linha[0]);
+        int quantidade = int.Parse(linha[1]);
+        double total = 0.0;
+
+        switch (codigo)
+        {
+            case 1:
+                total = 4.00 * quantidade;
+                break;
+            case 2:
+                total = 4.50 * quantidade;
+                break;
+            case 3:
+                total = 5.00 * quantidade;
+                break;
+            case 4:
+                total = 2.00 * quantidade;
+                break;
+            case 5:
+                total = 1.50 * quantidade;
+                break;
+        }
+
+        Console.WriteLine("Total: R$ {0:F2}", total);
+    }
+}
+
